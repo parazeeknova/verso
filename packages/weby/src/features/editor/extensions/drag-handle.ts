ff82e7817d596b912675a0bb9a5412f7ab133780
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-07-11 15:44:17 +0530

    style[EDITOR]: Align drag handle vertical position calculation with new height

--- packages/weby/src/features/editor/extensions/drag-handle.ts
+++ packages/weby/src/features/editor/extensions/drag-handle.ts
@@ -401,7 +401,7 @@ export const DragHandlePlugin = (options: GlobalDragHandleOptions & { pluginKey:
 
           const rect = absoluteRect(node);
 
-          rect.top += (lineHeight - 24) / 2;
+          rect.top += (lineHeight - 20) / 2;
           rect.top += paddingTop;
           // Li markers
           if (node.matches("ul:not([data-type=taskList]) li, ol li")) {
